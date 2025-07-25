1e6b2df9c1812b76ac9219e281b605adccfcde9f
svenviktorjonsson <svenviktorjonsson@gmail.com>
2025-07-25 11:32:00 +0200

    updated viteconfig and the sha katex hash

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/platonic-play/',
+  resolve: {
+    alias: {
+      'colormap-selector': new URL('./node_modules/colormap-selector/dist/index.js', import.meta.url).pathname
+    }
+  },
+  optimizeDeps: {
+    include: ['colormap-selector']
+  }
 });
