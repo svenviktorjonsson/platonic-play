f273b473161a03e7d42d0bc1733324889fe5b1de
svenviktorjonsson <svenviktorjonsson@gmail.com>
2025-07-25 13:52:59 +0200

    for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/platonic-play/',
+  base: '/', // This is correct for custom domain
   resolve: {
     alias: {
       'colormap-selector': resolve(process.cwd(), 'node_modules/colormap-selector')
     }
   }
-}); 
+});
