ad74f1c63af758533a61d290e781c3e541fa878d
svenviktorjonsson <svenviktorjonsson@gmail.com>
2025-07-25 11:39:38 +0200

    fixed vite config again

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/platonic-play/'
+  base: '/platonic-play/',
+  resolve: {
+    alias: {
+      'colormap-selector': new URL('./node_modules/colormap-selector', import.meta.url).pathname
+    }
+  }
 });
